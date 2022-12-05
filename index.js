beb8e45cdcd3c0516a140c251fb1bcb8b7b8949b
Heidy13cruz <heidy13cruz@gmail.com>
2022-12-05 09:41:05 -0600

    inicio

---
+++ index.js
@@ -0,0 +1,34 @@
+//Crear funcion para cambiar clase
+function cambiarClase() {
+    const titulo = document.getElementById("titulo");
+
+    const parrafo1 = document.querySelector("p")
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Crear funciones anonimas
+(a,b) => {
+
+}
+
+//Crear event listener 
+
